04b3234c1b1c7f6103508cc8a3742f964218968d
tomo-0131 <woovoodoor@yahoo.co.jp>
2022-01-29 14:09:01 +0900

    add: ChakraProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Header } from '../components/Header';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProvider>
       <Header />
       <Component {...pageProps} />
-    </>
+    </ChakraProvider>
   );
 }
 

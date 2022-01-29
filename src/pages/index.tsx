f514575349979cce1580ab97c3ec7199bc6e719b
tomo-0131 <woovoodoor@yahoo.co.jp>
2022-01-29 15:27:09 +0900

    add: SearchCharacters logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,39 @@ import { GetStaticProps } from 'next';
 import { useState } from 'react';
 import { Characters } from '../components/Characters';
 import { Props } from '../../type';
+import { useToast } from '@chakra-ui/react';
 
 const Home: React.FC<Props> = (results) => {
   const initialState = results;
-  console.log(initialState);
 
   const [characters, setCharacters] = useState(initialState.characters);
+
+  const [search, setSearch] = useState('');
+
+  console.log(search);
+
+  const toast = useToast();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const results = await fetch('/api/SearchCharacters', {
+      method: 'POST',
+      body: search,
+    });
+    const { characters, error } = await results.json();
+    if (error) {
+      toast({
+        position: 'top',
+        title: 'An error occurred.',
+        description: error,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      });
+    } else {
+      setCharacters(characters);
+    }
+  };
 
   return (
     <div className='mx-auto flex min-h-screen max-w-7xl flex-col items-center justify-center py-2'>
@@ -17,6 +44,38 @@ const Home: React.FC<Props> = (results) => {
         <title>Create Next App</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
+
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+        className='mt-7 mb-7'
+      >
+        <input
+          type='text'
+          placeholder='search'
+          value={search}
+          className='rounded-full border p-3'
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          type='button'
+          className='ml-4 mr-5 cursor-pointer'
+          onClick={() => {
+            setSearch('');
+            setCharacters(initialState.characters);
+          }}
+        >
+          ❌
+        </button>
+        <button
+          className='cursor-pointer rounded-lg border bg-yellow-500 p-2 font-bold text-white'
+          disabled={search === ''}
+          type='submit'
+        >
+          検索
+        </button>
+      </form>
 
       <div className='pb-12'>
         <Characters characters={characters} />
@@ -35,7 +94,7 @@ const Home: React.FC<Props> = (results) => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+export async function getStaticProps() {
   const client = new ApolloClient({
     uri: process.env.NEXT_PUBLIC_URI,
     cache: new InMemoryCache(),
@@ -76,7 +135,8 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       characters: data.characters.results,
     },
+    revalidate: 10,
   };
-};
+}
 
 export default Home;
